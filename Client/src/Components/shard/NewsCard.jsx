290c8f62166ea67f00bd1249c83092ce7120000f
Hasib Hossain <boraborhasib@gmail.com>
2023-04-19 07:52:55 +0600

    News Card Added

--- Client/src/Components/shard/NewsCard.jsx
+++ Client/src/Components/shard/NewsCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { formatDate } from '../../Utilities/formatedDate';
 import { AiOutlineShareAlt } from 'react-icons/ai'
-import { BiBookmark } from 'react-icons/bi'
+import { BiBookmark, BiStar } from 'react-icons/bi'
+import { FaEye } from 'react-icons/fa';
 // import { FaBookmark } from 'react-icons/fa';
 const NewsCard = ({ news }) => {
-    const { author, image_url, total_view, title, rating , details} = news;
+    const { author, image_url, total_view, title, rating, details } = news;
     return (
-        <div className='mb-4 border'>
+        <div className='mb-4 rounded-lg border'>
             <div className="title flex justify-between bg-[#F3F3F3] px-6 py-3">
                 <div className="flex items-center gap-3">
                     <img src={author.img} className='w-[40px] h-[40px]' alt="" />
@@ -23,7 +24,11 @@ const NewsCard = ({ news }) => {
             <div className="mx-5">
                 <h1 className='text-3xl font-bold my-4'>{title}</h1>
                 <img src={image_url} alt="" />
-                <p className='text-[#706F6F] my-5'>{details.slice(0,100)}...<span className='text-orange-500 font-bold'>Read More</span></p>
+                <p className='text-[#706F6F] my-5'>{details.slice(0, 100)}...<span className='text-orange-500 font-bold'>Read More</span></p>
+                <div className="flex justify-between mb-4">
+                    <p className='text-xl text-orange-400 flex items-center'><BiStar /><BiStar /><BiStar /><BiStar /><BiStar /> <span className='text-black ml-2'>{rating.number}</span></p>
+                    <p className='inline-flex items-center gap-1'><FaEye /> {total_view}</p>
+                </div>
             </div>
         </div>
     );
